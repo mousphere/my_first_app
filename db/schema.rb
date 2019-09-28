e1db76836a0528c686ddf7df4fe260e9ea935280
EC2 Default User <ec2-user@ip-172-31-33-24.us-east-2.compute.internal>
2019-09-28 01:49:48 +0000

    rename micropost article

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190927102944) do
+ActiveRecord::Schema.define(version: 20190928013533) do
 
-  create_table "microposts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "articles", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -20,8 +20,8 @@ ActiveRecord::Schema.define(version: 20190927102944) do
     t.string "area"
     t.string "genre"
     t.string "image"
-    t.index ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
-    t.index ["user_id"], name: "index_microposts_on_user_id"
+    t.index ["user_id", "created_at"], name: "index_articles_on_user_id_and_created_at"
+    t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -35,5 +35,5 @@ ActiveRecord::Schema.define(version: 20190927102944) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
-  add_foreign_key "microposts", "users"
+  add_foreign_key "articles", "users"
 end

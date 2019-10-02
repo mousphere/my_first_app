0ee0785b4bb814969a5c42a5752951569243fccc
EC2 Default User <ec2-user@ip-172-31-33-24.us-east-2.compute.internal>
2019-10-02 12:41:39 +0900

    change routes setteing

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,5 @@
 class ArticlesController < ApplicationController
   def new
-    @user = User.find(params[:user_id])
     @article = Article.new
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
 
   resources :users do
     get 'deactivate', on: :member
-    resources :articles
   end
+  
+  resources :articles
 end

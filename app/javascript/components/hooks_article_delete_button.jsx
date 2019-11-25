992770fd092addcd0a63caf044ef608c73d2908a
EC2 Default User <ec2-user@ip-172-31-33-24.us-east-2.compute.internal>
2019-11-25 15:59:23 +0900

    Modify article delete system with function of Hooks

--- app/javascript/components/hooks_article_delete_button.jsx
+++ app/javascript/components/hooks_article_delete_button.jsx
@@ -1,49 +1,80 @@
-import React, { useState } from "react"
+import React, { useState, useContext} from "react"
 import PropTypes from "prop-types"
 import classnames from 'classnames'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-  const HooksArticleDeleteButton = props => {
-    return <ArticleDeleteButton />
+  const hogeContext = React.createContext([
+    { isModalShown: false, deleteArticleID: null }, () => {}
+    ])
+
+  function HooksArticleDeleteButton(props){
+    const [value, setValue] = useState({ isModalShown: false, deleteArticleID: props.delete_article.id })
+    // alert(value.deleteArticleID)
+    return(
+           <hogeContext.Provider value={[value, setValue]} >
+             <ArticleDeleteButton />
+           </hogeContext.Provider>
+           )
   }
   
-  const ArticleDeleteButton = props => {
-    const [showPopup, setShowPopup] = useState(false)
+  function ArticleDeleteButton(props){
     
     const className = classnames('btn btn-link icon-big')
+    const [value, setValue] = useContext(hogeContext)
+
     return (
       <div>
         <button
         className={ className }
-        onClick={() => setShowPopup(true)}>
+        onClick={() => setValue({ isModalShown: true }) } >
         <FontAwesomeIcon icon={['far', 'trash-alt']} />
         </button>
 
-        {showPopup ? <Popup /> : null }
+        {value.isModalShown ? <Modal /> : null }
       </div>
     )
   }
-  
-  const Popup = props => {
+
+  function Modal(props){
+    const [value, setValue] = useContext(hogeContext)
+    
+    const deleteArticle = () =>{
+      // setValue({deleteArticleID: 2 })
+      // alert(value.deleteArticleID)
+      return new Promise((resolve, reject) => {
+        $.ajax({
+          type: 'DELETE',
+          url: `/articles/${value.deleteArticleID}`,
+          dataType: 'json',
+          contentType: 'application/json',
+          beforeSend: function(xhr) {
+            xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content'))
+          }
+        })
+        resolve()
+      })
+    }
+    
     return (
       <div className='popup'>
         <div className='popup_inner text-center'>
           <b>この記事を削除しますか？</b>
           <p>この操作は取り消せません。このサイト上全てのページで表示されていた記事が削除されます。</p>
           <div className='modal-button'>
-            <button className='btn btn-light'>キャンセル</button>
-            <button className='btn btn-danger'>削除</button>
+            <button className='btn btn-light' onClick={() => setValue({ isModalShown: false })}>キャンセル</button>
+            <button className='btn btn-danger' onClick={() => deleteArticle().then(setValue({ isModalShown: false })) } >削除</button>
           </div>
         </div>
       </div>
     )
   }
-
+  
 export default HooksArticleDeleteButton
-// ArticleDeleteButton.defaultProps = {
-//   delete_article: null
-// }
 
-// ArticleDeleteButton.propTypes = {
-//   delete_article: PropTypes.object.isRequired
-// }
+HooksArticleDeleteButton.defaultProps = {
+  delete_article: null
+}
+
+HooksArticleDeleteButton.propTypes = {
+  delete_article: PropTypes.object.isRequired
+}

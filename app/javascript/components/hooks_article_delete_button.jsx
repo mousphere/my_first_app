e3b29ecc41cec73bb070ba570f1d0673fad3149f
EC2 Default User <ec2-user@ip-172-31-33-24.us-east-2.compute.internal>
2019-11-26 21:57:53 +0900

    Modify article delete system using props

--- app/javascript/components/hooks_article_delete_button.jsx
+++ app/javascript/components/hooks_article_delete_button.jsx
@@ -1,50 +1,32 @@
-import React, { useState, useContext} from "react"
+import React, { useState } from "react"
 import PropTypes from "prop-types"
 import classnames from 'classnames'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-  const hogeContext = React.createContext([
-    { isModalShown: false, deleteArticleID: null }, () => {}
-    ])
-
-  function HooksArticleDeleteButton(props){
-    const [value, setValue] = useState({ isModalShown: false, deleteArticleID: props.delete_article.id })
-    // alert(value.deleteArticleID)
-    return(
-           <hogeContext.Provider value={[value, setValue]} >
-             <ArticleDeleteButton />
-           </hogeContext.Provider>
-           )
-  }
-  
   function ArticleDeleteButton(props){
-    
+    const [isModalShown, setValue] = useState(false)
+    const [deleteArticleID, setID] = useState(props.delete_article.id)
     const className = classnames('btn btn-link icon-big')
-    const [value, setValue] = useContext(hogeContext)
 
     return (
       <div>
         <button
-        className={ className }
-        onClick={() => setValue({ isModalShown: true }) } >
-        <FontAwesomeIcon icon={['far', 'trash-alt']} />
+          className={ className }
+          onClick={() => setValue(true) } >
+          <FontAwesomeIcon icon={['far', 'trash-alt']} />
         </button>
-
-        {value.isModalShown ? <Modal /> : null }
+        { isModalShown ? <Modal isModalShown = {isModalShown} setValue = {setValue}
+                                deleteArticleID = {deleteArticleID} /> : null }
       </div>
     )
   }
 
-  function Modal(props){
-    const [value, setValue] = useContext(hogeContext)
-    
+  function Modal({ isModalShown, setValue, deleteArticleID }){
     const deleteArticle = () =>{
-      // setValue({deleteArticleID: 2 })
-      // alert(value.deleteArticleID)
       return new Promise((resolve, reject) => {
         $.ajax({
           type: 'DELETE',
-          url: `/articles/${value.deleteArticleID}`,
+          url: `/articles/${deleteArticleID}`,
           dataType: 'json',
           contentType: 'application/json',
           beforeSend: function(xhr) {
@@ -55,26 +37,50 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
       })
     }
     
+    const hideModal = () =>{
+      return new Promise((resolve, reject) => {
+        setValue(false)
+        resolve()
+      })
+    }
+    
+    const reloadPage = () =>{
+      return new Promise((resolve, reject) => {
+        window.location.reload(true)
+        resolve()
+      })
+    }
+    
     return (
       <div className='popup'>
         <div className='popup_inner text-center'>
           <b>この記事を削除しますか？</b>
           <p>この操作は取り消せません。このサイト上全てのページで表示されていた記事が削除されます。</p>
           <div className='modal-button'>
-            <button className='btn btn-light' onClick={() => setValue({ isModalShown: false })}>キャンセル</button>
-            <button className='btn btn-danger' onClick={() => deleteArticle().then(setValue({ isModalShown: false })) } >削除</button>
+            <button
+              className='btn btn-light'
+              onClick={ () => setValue(false) }>
+              キャンセル
+            </button>
+            <button
+              className='btn btn-danger' 
+              onClick={ () => deleteArticle()
+                              .then(hideModal)
+                              .then(reloadPage) } >
+              削除
+            </button>
           </div>
         </div>
       </div>
     )
   }
   
-export default HooksArticleDeleteButton
+export default ArticleDeleteButton
 
-HooksArticleDeleteButton.defaultProps = {
+ArticleDeleteButton.defaultProps = {
   delete_article: null
 }
 
-HooksArticleDeleteButton.propTypes = {
+ArticleDeleteButton.propTypes = {
   delete_article: PropTypes.object.isRequired
 }

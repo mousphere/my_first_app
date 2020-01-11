7b90517ebd83529e37ecf3d5a745a7ad5aa19cb1
EC2 Default User <ec2-user@ip-172-31-33-24.us-east-2.compute.internal>
2020-01-11 15:46:21 +0900

    failure

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191221053300) do
+ActiveRecord::Schema.define(version: 20200111052747) do
 
   create_table "articles", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "content"

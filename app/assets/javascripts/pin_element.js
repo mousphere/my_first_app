e61f2141ee8e5c0f49d26aca6ae96cf0c6d1e78b
EC2 Default User <ec2-user@ip-172-31-33-24.us-east-2.compute.internal>
2020-02-09 17:52:45 +0900

    Fix destroyed view

--- app/assets/javascripts/pin_element.js
+++ app/assets/javascripts/pin_element.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(window).on('load', function(){
   const button = $('#changing_display_order_button');
   const naviParent = $('#naviParent');
   const navbar = $('.navbar');
